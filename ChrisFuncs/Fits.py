a1d5ae38bfe16b1661b12d0beb82040f22aa6125
Chris Clark <cjrc88@gmail.com>
2019-04-09 15:16:37 -0400

    Created MontageWrapperWrapper, to call Montage in same way as reproject
    The awful MontageWrapperWrapperWrapper solution exists to do most of the work in a multiprocessing process, so that any temporary directories it makes can be deleted, even if they are on network drives, because the .nfs lock file will have been removed when the spawned process wrapped up.

--- ChrisFuncs/Fits.py
+++ ChrisFuncs/Fits.py
@@ -8,8 +8,12 @@ import numpy as np
 import astropy.io.fits
 import astropy.wcs
 import astropy.convolution
+import multiprocessing as mp
 import aplpy
 import reproject
+import tempfile
+import shutil
+import time
 from ChrisFuncs import SigmaClip, Nanless
 
 # Handle the lack of the basestring class in Python 3
@@ -265,6 +269,74 @@ def FitsRGB(ra, dec, rad_arcsec, in_paths, out_dir, pmin=False, pmax=False, stre
 
     # Clean up temporary files
     [os.remove(path_zoomed) for path_zoomed in path_zoomed_list]
+
+
+
+# Define function to run montage_wrapper in a temp file, and tidy up when done (a Montate wrapper wrapper, if you will)
+# Inputs: FITS data to be reprojected, being either an astropy.io.fits.HDU object, or path to FITS file; header to reproject to, being either an astropy.io.fits.Header object, or a string to an mHdr-type text file; (path to directory holding Montage commands, in case this is not already part of the system PATH; path to directory to place temporary files in, in case you have a strong perference in this regard)
+# Returns: Array of reprojected data
+def MontageWrapperWrapper(in_fitsdata, in_hdr, montage_path=None, temp_path=None):
+
+    # Define inner function, so we can do everything else inside a process
+    def MontageWrapperWrapperWrapper(in_fitsdata, in_hdr, manager, montage_path=None, temp_path=None):
+
+        # Handle Montage path, if kwargs provided
+        if montage_path != None:
+            os.environ['PATH'] += ':'+montage_path
+        import montage_wrapper
+
+        # Produce on-the-fly temporary file
+        timestamp = str(time.time()).replace('.','-')
+        if isinstance(temp_path, str):
+            temp_dir = tempfile.mkdtemp(dir=temp_path)
+        else:
+            temp_dir = tempfile.mkdtemp()
+
+        # If FITS data provided is path to file, record that path; else if FITS data provided is an astropy HDUList object, write it to the temporary directory
+        if isinstance(in_fitsdata, basestring):
+            if os.path.exists(in_fitsdata):
+                in_fits_path = in_fitsdata
+            else:
+                raise Exception('No FITS file at path provided')
+        elif isinstance(in_fitsdata, astropy.io.fits.hdu.image.PrimaryHDU) or isinstance(in_fitsdata, astropy.io.fits.hdu.image.ImageHDU):
+            in_fits_path = os.path.join(temp_dir,'temp_in_'+timestamp+'.fits')
+            in_fitsdata = astropy.io.fits.HDUList([in_fitsdata])
+            in_fitsdata.writeto(in_fits_path)
+
+        # If header provided is path to file, record that path; else if header previded is a astropy Header object, write it to the temporary directory
+        if isinstance(in_hdr, basestring):
+            if os.path.exists(in_hdr):
+                hdr_path = in_hdr
+            else:
+                raise Exception('No header file at path provided')
+        elif isinstance(in_hdr, astropy.io.fits.Header):
+            hdr_path = os.path.join(temp_dir,'temp_header_'+timestamp+'.hdr')
+            in_hdr.totextfile(hdr_path)
+
+        # Reproject data with montage_wrapper
+        out_fits_path = os.path.join(temp_dir,'temp_out_'+timestamp+'.fits')
+        montage_wrapper.reproject(in_fits_path, out_fits_path, hdr_path, exact_size=True)
+        out_img = astropy.io.fits.getdata(out_fits_path)
+
+        # Return output image array, and path to temporary directory
+        manager['out_img'] = out_img
+        manager['temp_dir'] = temp_dir
+
+    # Run inner function, as defined above, in a process, with results passed to a manager dictionary
+    manager = mp.Manager().dict()
+    process = mp.Process(target=MontageWrapperWrapperWrapper, args=(in_fitsdata, in_hdr, manager,), kwargs={'montage_path':montage_path,'temp_path':temp_path})
+    process.start()
+    process.join()
+
+    # Grab outputs from manager dictionary
+    out_img = manager['out_img']
+    temp_dir = manager['temp_dir']
+
+    # Delete temporary folder
+    shutil.rmtree(temp_dir)
+
+    # Return output array
+    return out_img
 
 
 
